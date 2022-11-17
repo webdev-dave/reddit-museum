befe5e37b1d163991c3b8a331c04d3d73ad27d32
David Richard <elidovrich.coding@gmail.com>
2022-11-17 16:02:35 -0500

    add npm saveAs package for download func

--- src/components/posts/postComponents/PostMenu.js
+++ src/components/posts/postComponents/PostMenu.js
@@ -1,17 +1,17 @@
 // import { useState } from "react";
 import { FaDownload } from "react-icons/fa";
+import { saveAs } from "file-saver";
 // import { sortGallery } from "../../../utils/helperFunctions";
 
-
-const PostMenu = ({post}) => {
+const PostMenu = ({ post }) => {
   //const [downloadUrl, setDownloadUrl] = useState(post.srcUrl);
   const credits = post.credits;
   //const gallery = isGallery && sortGallery(child.redditGalleryOrder, child.initialGallery);
+  const downloadMedia = () => {
+    saveAs(post.srcUrl, "test.jpg")
+    //saveAs("use_current_media_url", "use_current_post_title")
+  }
 
-
-  // if(isVideo){
-  //   setDownloadUrl(videoUrl);
-  // } 
 
   return (
     <div>
@@ -28,11 +28,12 @@ const PostMenu = ({post}) => {
         >
           Origin
         </a>
-        <button>
-          <a href={post.srcUrl} download>
-            <FaDownload className="download-icon" />
-          </a>
-          
+
+        {/* <a href={post.srcUrl} download onClick={(e) => download(e)}>
+          <FaDownload className="download-icon" />
+        </a> */}
+        <button onClick={downloadMedia}>
+          <FaDownload className="download-icon" />
         </button>
       </div>
     </div>
--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -22,3 +22,4 @@ export const isHostedOnReddit = (isGallery, child) => {
     return false;
   }
 };
+
