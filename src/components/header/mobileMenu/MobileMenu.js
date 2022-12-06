c74357eae9d9e67fc739e0c4815f0a12305b7d1e
David Richard <elidovrich.coding@gmail.com>
2022-12-05 20:53:29 -0500

    test mobile menu height lag fix 2

--- src/components/header/mobileMenu/MobileMenu.js
+++ src/components/header/mobileMenu/MobileMenu.js
@@ -8,13 +8,18 @@ import NavBar from "../navBar/NavBar";
 const MobileMenu = () => {
   const [isExpanded, setIsExpanded] = useState(false);
   const [vh, setVh] = useState(window.innerHeight * 0.01);
-  console.log((vh*100)-80);
+  const [menuWidth, setMenuWidth] = useState(0);
+  const dropdownRef = useRef();
   const headerHeight = "80";
   const sideMenuRef = useRef();
   const expandedClassName = isExpanded ? "expanded" : "";
 
+  useEffect(()=>{
+    setMenuWidth(dropdownRef.current.offsetWidth);
+  },[])
+
   useEffect(() => {
-    const handleResize = () => setVh(window.innerHeight * 0.01);
+    const handleResize = () =>  setVh(window.innerHeight * 0.01);
     window.addEventListener("resize", handleResize);
     return () => document.removeEventListener("resize", handleResize);
   });
@@ -39,11 +44,21 @@ const MobileMenu = () => {
           {isExpanded ? <FaTimes /> : <FaBars />}
         </button>
         <div
+        ref={dropdownRef}
           className={`dropdown ${expandedClassName}`}
-          // style={{ height: `100${vh}px` }}
-          style={{ height: `calc(${100*vh}px - ${headerHeight}px)`, marginBottom: `${10*vh}px` }}
+          style={{ height: `calc(${100*vh}px - ${headerHeight}px)` }}
         >
           <NavBar isColumn={true} />
+          {/* <div style={{ height: `calc(${150*vh}px - ${headerHeight}px)` }}>
+            
+          </div> */}
+          
+        </div>
+        <div
+          className={`dropdown-length-extender ${expandedClassName}`}
+          style={{ height: `${200*vh}px`, width: `${menuWidth}px`,  }}
+        >
+          
         </div>
       </div>
   
