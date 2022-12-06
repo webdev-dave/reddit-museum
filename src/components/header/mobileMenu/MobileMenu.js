10d34e9673f49542015de935f48de17ad12b85dd
David Richard <elidovrich.coding@gmail.com>
2022-12-05 21:27:13 -0500

    test mobile menu height lag fix 4

--- src/components/header/mobileMenu/MobileMenu.js
+++ src/components/header/mobileMenu/MobileMenu.js
@@ -8,18 +8,13 @@ import NavBar from "../navBar/NavBar";
 const MobileMenu = () => {
   const [isExpanded, setIsExpanded] = useState(false);
   const [vh, setVh] = useState(window.innerHeight * 0.01);
-  const [menuWidth, setMenuWidth] = useState(0);
-  const dropdownRef = useRef();
+
   const headerHeight = "80";
   const sideMenuRef = useRef();
   const expandedClassName = isExpanded ? "expanded" : "";
 
-  useEffect(()=>{
-    setMenuWidth(dropdownRef.current.offsetWidth);
-  },[])
-
   useEffect(() => {
-    const handleResize = () =>  setVh(window.innerHeight * 0.01);
+    const handleResize = () => setVh(window.innerHeight * 0.01);
     window.addEventListener("resize", handleResize);
     return () => document.removeEventListener("resize", handleResize);
   });
@@ -35,33 +30,27 @@ const MobileMenu = () => {
   });
 
   return (
-
-      <div className="side-menu-container" ref={sideMenuRef}>
-        <button
-          onClick={() => setIsExpanded(!isExpanded)}
-          className={expandedClassName}
-        >
-          {isExpanded ? <FaTimes /> : <FaBars />}
-        </button>
+    <div className="side-menu-container" ref={sideMenuRef}>
+      <button
+        onClick={() => setIsExpanded(!isExpanded)}
+        className={expandedClassName}
+      >
+        {isExpanded ? <FaTimes /> : <FaBars />}
+      </button>
+      <div
+        className={`nav-bar-container ${expandedClassName}`}
+        // style={{ height: `calc(${100*vh}px - ${headerHeight}px)` }}
+      >
         <div
-        ref={dropdownRef}
-          className={`dropdown ${expandedClassName}`}
-          style={{ height: `calc(${100*vh}px - ${headerHeight}px)` }}
+          className="nav-bar-wrapper"
+          style={{ height: `calc(${100 * vh}px - ${headerHeight}px)` }}
         >
           <NavBar isColumn={true} />
-          {/* <div style={{ height: `calc(${150*vh}px - ${headerHeight}px)` }}>
-            
-          </div> */}
-          
         </div>
-        <div
-          className={`dropdown-length-extender ${expandedClassName}`}
-          style={{ height: `${200*vh}px`, width: `${menuWidth}px`,  }}
-        >
-          
-        </div>
+
+        <div className="extra-length"></div>
       </div>
-  
+    </div>
   );
 };
 
