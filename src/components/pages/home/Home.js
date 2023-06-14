d385f58247be2a27093afa0f22f8daef0ed741e7
David Richard <elidovrich.coding@gmail.com>
2023-06-14 16:01:54 -0400

    update mainBanner to warn about ongoing reddit blackouts

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,15 +1,15 @@
-// import MainBanner from "../../mainBanner/MainBanner";
-import { Navigate } from "react-router-dom";
+import MainBanner from "./mainBanner/MainBanner";
+//import { Navigate } from "react-router-dom";
 
 const Home = () => {
-  return (<Navigate to="/photography/film" />);
-  // return (
-  //   <div className="home">
-  //     <main className="posts-section">
-  //       <MainBanner />
-  //     </main>
-  //   </div>
-  // );
+  // return (<Navigate to="/photography/film" />);
+  return (
+    <div className="home">
+      <main className="posts-section">
+        <MainBanner />
+      </main>
+    </div>
+  );
 };
 
 export default Home;
