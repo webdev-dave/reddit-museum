8ac8147ab2c86f1af1d3b9adc74a7d4db0cfc755
David Richard <elidovrich.coding@gmail.com>
2023-08-09 12:48:21 -0400

    update homepage reroute to mixed_paintings

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -2,7 +2,7 @@
 import { Navigate } from "react-router-dom";
 
 const Home = () => {
-  return (<Navigate to="/photography/film" />);
+  return (<Navigate to="/paintings/mixed_paintings" />);
   // return (
   //   <div className="home">
   //     <main className="posts-section">
