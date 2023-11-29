43bbc929e5570029c31ebf19284f2dc16d640cfc
David Richard <elidovrich.coding@gmail.com>
2023-11-29 15:08:55 -0500

    modified h1 header to appear above video banner

--- src/components/pages/home/mainBanner/MainBanner.js
+++ src/components/pages/home/mainBanner/MainBanner.js
@@ -1,23 +1,24 @@
 //import headerImg from "../../../../assets/localMedia/headerImg.png";
 import videoBanner from "../../../../assets/localMedia/video_banner.mp4"
 import "./mainBanner.css";
-import { FaExclamationTriangle } from "react-icons/fa";
+// import { FaExclamationTriangle } from "react-icons/fa";
 
 const MainBanner = () => {
   return (
     <div className="main-banner">
-      <h1>Reddit Museum</h1>
+
       {/* <img src={headerImg} alt="" /> */}
-      <video autoPlay={"autoplay"} muted loop width="100%" height="" >
+      <video id="videoBanner" autoPlay={"autoplay"} muted loop width="100%" height="" >
       <source src={videoBanner} type="video/mp4"/>
      </video>
-      <div className="userAlert">
+     <h1>Welcome to the Reddit Museum</h1>
+      {/* <div className="userAlert">
         <FaExclamationTriangle className="icon" />
         <p>
           Project is currently in development mode and is updated frequently.
           All features are subject to sudden changes.
         </p>
-      </div>
+      </div> */}
     </div>
   );
 };
