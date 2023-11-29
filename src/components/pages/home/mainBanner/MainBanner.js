db76be50a7a00b635d55d792d0ac14caeb51761d
David Richard <elidovrich.coding@gmail.com>
2023-11-29 15:24:49 -0500

    crop video banner

--- src/components/pages/home/mainBanner/MainBanner.js
+++ src/components/pages/home/mainBanner/MainBanner.js
@@ -1,17 +1,28 @@
 //import headerImg from "../../../../assets/localMedia/headerImg.png";
-import videoBanner from "../../../../assets/localMedia/video_banner.mp4"
+import videoBanner from "../../../../assets/localMedia/video_banner.mp4";
 import "./mainBanner.css";
 // import { FaExclamationTriangle } from "react-icons/fa";
 
 const MainBanner = () => {
   return (
     <div className="main-banner">
-
       {/* <img src={headerImg} alt="" /> */}
-      <video id="videoBanner" autoPlay={"autoplay"} muted loop width="100%" height="" >
-      <source src={videoBanner} type="video/mp4"/>
-     </video>
-     <h1>Welcome to the Reddit Museum</h1>
+      <div id="videoContainer">
+        <video
+          id="videoBanner"
+          autoPlay={"autoplay"}
+          muted
+          loop
+          width="100%"
+          height=""
+        >
+          <source src={videoBanner} type="video/mp4" />
+        </video>
+      </div>
+
+      <div id="headerContainer">
+        <h1>Welcome to the Reddit Museum</h1>
+      </div>
       {/* <div className="userAlert">
         <FaExclamationTriangle className="icon" />
         <p>
