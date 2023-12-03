99a8925ac7bbe0c87acfadbefff408f165aec6b4
David Richard <elidovrich.coding@gmail.com>
2023-12-03 17:35:25 -0500

    begin building horizontal carousel for galleriesShelf

--- src/components/pages/home/galleriesShelf/GalleriesShelf.js
+++ src/components/pages/home/galleriesShelf/GalleriesShelf.js
@@ -1,10 +1,18 @@
 import "./galleriesShelf.css";
 
 const GalleriesShelf = () => {
+  const galleryFrame = <li className="galleryFrame"></li>;
+  const galleryFrames = [];
+  for (let i = 0; i < 5; i++) {
+    galleryFrames.push(galleryFrame);
+  }
+  console.log(galleryFrames);
   return (
     <div className="shelfWrapper">
       <h2>Now on View</h2>
-      <div className="galleriesShelf"></div>
+      <ul className="galleriesShelf">{galleryFrames.map((gf, index) => (
+        <li key={"galleryFrame-" + index}>{gf}</li>
+      ))}</ul>
     </div>
   );
 };
