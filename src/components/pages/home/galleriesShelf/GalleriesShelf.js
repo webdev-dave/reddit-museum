3b52d6c23d3126deec2f8f361d874e5493be0f00
David Richard <elidovrich.coding@gmail.com>
2023-12-06 12:57:51 -0500

    add sliderButton functionality

--- src/components/pages/home/galleriesShelf/GalleriesShelf.js
+++ src/components/pages/home/galleriesShelf/GalleriesShelf.js
@@ -1,29 +1,39 @@
 import "./galleriesShelf.css";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
-//import { useState, useRef } from "react";
-
-//solution for building a scroller lies in using document.getElementById("slider").scrollLeft += 20;
+import { useRef } from "react";
 
 const GalleriesShelf = () => {
   const slide = "slide";
   const slides = [];
-  for (let i = 0; i < 5; i++) {
+  const slideWidth = 400;
+  for (let i = 0; i < 7; i++) {
     slides.push(slide);
+  };
+  const sliderRef = useRef();
+
+  const handleRightClick = () => {
+    sliderRef.current.scrollLeft += slideWidth;
   }
+
+  const handleLeftClick = () => {
+    sliderRef.current.scrollLeft -= slideWidth;
+  }
+
+
   console.log(slides);
   return (
     <div className="shelfContainer">
       <h2>Now on View</h2>
       <div className="sliderContainer">
-        <button id="slideLeft">{<FaAngleLeft className="icon" />}</button>
-        <ul className="slider">
+        <button id="slideLeft" onClick={handleLeftClick}>{<FaAngleLeft className="icon" />}</button>
+        <ul className="slider" ref={sliderRef}>
           {slides.map((s, index) => (
             <li className="slide" key={"slide-" + index}>
               <p>{s}</p>
             </li>
           ))}
         </ul>
-        <button id="slideRight">{<FaAngleRight className="icon" />}</button>
+        <button id="slideRight" onClick={handleRightClick}>{<FaAngleRight className="icon" />}</button>
       </div>
     </div>
   );
